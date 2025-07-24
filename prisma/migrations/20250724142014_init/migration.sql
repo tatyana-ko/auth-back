c09f8a1954938f39b1e1b75bc706d8154898d1f9
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-24 17:26:13 +0300

    feat: initialized prisma and add  user model

---
+++ prisma/migrations/20250724142014_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar_path" TEXT,
+    "rights" "Role"[] DEFAULT ARRAY['USER']::"Role"[],
+    "otp_code" TEXT,
+    "otp_expires_at" TIMESTAMP(3),
+    "telegram_id" TEXT,
+    "verification_token" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_name_key" ON "users"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
